a9a21e374302f545bc2de12a573953adcaebdd48
w1n5rx <vinod.ramalwan@gmail.com>
2021-04-02 10:11:40 +0200

    chore: modify page app

--- apps/page/src/app/app.spec.tsx
+++ apps/page/src/app/app.spec.tsx
@@ -13,6 +13,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('Welcome to page!')).toBeTruthy();
+    expect(getByText('This is a Page!')).toBeTruthy();
   });
 });
