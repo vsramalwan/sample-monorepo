6e5f9717bd0bbf1baa3684e0c1fa5929ce26b0da
vsramalwan <vinod.ramalwan@gmail.com>
2021-04-04 09:10:27 +0200

    refactor: page-lib component

--- libs/page-lib/src/lib/page-lib.tsx
+++ libs/page-lib/src/lib/page-lib.tsx
@@ -2,15 +2,4 @@ import React from 'react';
 
 import './page-lib.module.css';
 
-/* eslint-disable-next-line */
-export interface PageLibProps {}
-
-export function PageLib(props: PageLibProps) {
-  return (
-    <div>
-      <h1>This is a Page-lib!</h1>
-    </div>
-  );
-}
-
-export default PageLib;
+export const PageLib = ():JSX.Element => <h1>This is a Page-lib!</h1>
